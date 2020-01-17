4881d3b29350001f964b2320f52ee7e9424d83ec
Dima <katri4dimon@i.ua>
2020-01-17 16:18:45 +0200

    create component BarBanner

---
+++ src/App/Main/Sidebar/BarBanner/BarBanner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BarBanner = () => {
+    return (
+        <div className="sidebar-block bar-banner">
+            <div className="banner-img">
+                <img src="images/banner.png" alt="banner" />
+                <div className="banner-text">Banner spot</div>
+            </div>
+		</div>
+    )
+}
+
+export default BarBanner
