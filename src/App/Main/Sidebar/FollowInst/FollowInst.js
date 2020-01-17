55c0d0c254729846d1d380de0964af39032249ea
Dima <katri4dimon@i.ua>
2020-01-17 16:25:32 +0200

    create component FollowInst

---
+++ src/App/Main/Sidebar/FollowInst/FollowInst.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FollowInst = () => {
+    return (
+        <div className="sidebar-block folllow-inst">
+            <div className="bar-title">Follow on instagram</div>
+            <div className="img-instagram">
+                <a href="/"><img src="images/img_inst1.png" alt="img_inst1"/></a>
+                <a href="/"><img src="images/img_inst2.png" alt="img_inst2"/></a>
+                <a href="/"><img src="images/img_inst3.png" alt="img_inst3"/></a>
+                <a href="/"><img src="images/img_inst4.png" alt="img_inst4"/></a>
+            </div>
+		</div>
+    )
+}
+
+export default FollowInst
--- src/App/Main/Sidebar/Sidebar.js
+++ src/App/Main/Sidebar/Sidebar.js
@@ -4,6 +4,7 @@ import Categories from './Categories/Categories'
 import Subscribe from './Subscribe/Subscribe'
 import BarBanner from './BarBanner/BarBanner'
 import PopularPosts from './PopularPosts/POpularPosts'
+import FollowInst from './FollowInst/FollowInst'
 
 const Sidebar = () => {
     return (
@@ -13,15 +14,7 @@ const Sidebar = () => {
 				<Subscribe/>
 				<BarBanner/>
 				<PopularPosts/>
-				<div className="sidebar-block folllow-inst">
-					<div className="bar-title">Follow on instagram</div>
-					<div className="img-instagram">
-						<a href="/"><img src="images/img_inst1.png" alt="img_inst1"/></a>
-						<a href="/"><img src="images/img_inst2.png" alt="img_inst2"/></a>
-						<a href="/"><img src="images/img_inst3.png" alt="img_inst3"/></a>
-						<a href="/"><img src="images/img_inst4.png" alt="img_inst4"/></a>
-					</div>
-				</div>
+				<FollowInst/>
 				<div className="sidebar-block popular-tags">
 					<div className="bar-title">Popular tags</div>
 					<div className="tags">
